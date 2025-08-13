f5f73fdd43d4050cd51f1b7738596094ddcd8e2f
jaceybarra <jaceybarra23@gmail.com>
2025-08-12 22:40:28 -0600

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import path from 'path';
 
-const DB_PATH = path.join(process.cwd(), 'data.db');
+const DB_PATH = process.env.DB_PATH || path.join(process.cwd(), 'data.db');
 
 export async function getDb() {
   const db = await open({ filename: DB_PATH, driver: sqlite3.Database });
